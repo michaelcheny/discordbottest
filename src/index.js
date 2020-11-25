2bab13bf2240d2bcb4e881393c996bf27f0ce700
Michael Chen <michaelchengaming@gmail.com>
2020-11-25 15:31:30 -0800

    add kick command and catches for errors and no permissions

--- src/index.js
+++ src/index.js
@@ -13,24 +13,29 @@ client.on('ready', () => {
 client.on('message', (msg) => {
   if (msg.author.bot || !msg.content.startsWith(PREFIX)) return;
 
-  const [commandName, ...args] = msg.content.trim().substring(PREFIX.length).split(/\s+/);
+  const [command, ...args] = msg.content.trim().substring(PREFIX.length).split(/\s+/);
 
-  // console.log(commandName);
+  // console.log(command);
   // console.log(args);
 
-  if (commandName === 'kick') {
+  if (command === 'kick') {
+    if (!msg.member.hasPermission('KICK_MEMBERS'))
+      return message.reply('You do not have permissions to use that command.');
     if (args.length === 0) return msg.reply('Please provide ID');
     const member = msg.guild.members.cache.get(args[0]);
-    console.log(msg.guild.members.cache);
+    console.log(msg.guild.members.cache.get(args[0]));
 
-    if (member) member.kick();
-    msg.channel.send('Bye felicia');
-    // implement kick from server
-  } else if (commandName === 'ban') {
-    msg.channel.send('banned');
-    // implement ban from server
-  } else {
-    // fall back for no command found
+    if (member) {
+      member
+        .kick()
+        .then((member) => msg.channel.send(`Bye ${member}.`))
+        .catch((err) => message.channel.send('This member is an admin. No can do.'));
+    } else if (command === 'ban') {
+      msg.channel.send('banned');
+      // implement ban from server
+    } else {
+      // fall back for no command found
+    }
   }
 
   // console.log(`[${msg.author.tag}]: ${msg.content}`);
